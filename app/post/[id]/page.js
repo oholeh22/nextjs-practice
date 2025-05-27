89fbc7bf931cad73981721dae5275500fdff8b6b
Oleh Shyian <shiyan4242@gmail.com>
2025-05-27 16:03:40 +0200

    export project out serve

--- app/post/[id]/page.js
+++ app/post/[id]/page.js
@@ -3,17 +3,24 @@ import Post from '@/app/components/Post';
 export async function generateMetadata({ params }) {
   const post = await fetchData(params.id);
   return {
-    title: post.title + ' - Page on site ',
+    title: `${post.title} - Page on site`,
     description: post.body,
   };
 }
 
+export async function generateStaticParams() {
+  const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+  const posts = await res.json();
+
+  return posts.map(post => ({
+    id: post.id.toString(),
+  }));
+}
+
 async function fetchData(id) {
   const res = await fetch('https://jsonplaceholder.typicode.com/posts/' + id, {
-    cache: 'no-store',
   });
-  const result = await res.json();
-  return result;
+  return res.json();
 }
 
 const PagePost = async ({ params: { id } }) => {
